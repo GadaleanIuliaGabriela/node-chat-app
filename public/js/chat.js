910db18a9c5a5efb029af4a4da2f7ca886ccb9e2
Iulia Gadalean <gadalean.iulia.gabriela@gmail.com>
2021-11-01 14:54:08 +0200

    Render dynamic messages.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,29 +1,58 @@
 const socket = io()
 
-const messageFrom = document.querySelector('#message-form')
+// Elements
+const $messageFrom = document.querySelector('#message-form')
+const $messageFromInput = $messageFrom.querySelector('input')
+const $messageFromButon = $messageFrom.querySelector('button')
+const $locationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
 })
 
-messageFrom.addEventListener('submit', (e) => {
+$messageFrom.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    // disable the button
+    $messageFromButon.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message, (message) => {
-        console.log("The message was delivered!", message)
+    socket.emit('sendMessage', message, (error) => {
+        // enable
+        $messageFromButon.removeAttribute('disabled')
+        $messageFromInput.value = ''
+        $messageFromInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log("Message delivered!")
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$locationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+
+    $locationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, () => {
+            $locationButton.removeAttribute('disabled')
+            console.log("Location shared!")
         })
     })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -24,14 +25,20 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed!')
+        }
+
         // se trimite la toti
         io.emit('message', message)
-
-        callback('Delivered!')
+        callback()
     })
 
-    socket.on('sendLocation', (location) => {
+    socket.on('sendLocation', (location, callback) => {
         io.emit('message', 'https://google.com/maps?q=' + location.latitude+","+location.longitude)
+        callback()
     })
 
     // are loc atunci cand un client se deconecteaza
