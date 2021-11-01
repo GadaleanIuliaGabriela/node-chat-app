f1689698caf206fdb4166d34aeb9c26b1f62c4a1
Iulia Gadalean <gadalean.iulia.gabriela@gmail.com>
2021-11-01 12:30:54 +0200

    emit and listen

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,15 @@
-io()
+const socket = io()
+
+//aici primim eventul
+socket.on('countUpdated', (count) => {
+    console.log("The count has been updated!", count)
+})
+
+//detectam evenimentul click
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked')
+
+    // emitem un event atunci cand se apasa butonul
+    socket.emit('increment')
+})
+
--- src/index.js
+++ src/index.js
@@ -13,13 +13,26 @@ const publicDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectory))
 
-
+let count = 0
 // event care se numeste connection si care are loc atunci cand un client se conecteaza la server
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New wev socket connection')
+    // cand lucram cu sokcetio transferam date(trimitem/primim) prin events
+    // aici trimitem un event de la server si vrem sa primim eventul pe client
+    // aici folosim un event custom
+    socket.emit('countUpdated', count)
 
+    socket.on('increment', () => {
+        count++
 
+        // nu functioneaza pentru ca trimite raspunsul numai la socket-ul actual
+        // socket.emit('countUpdated', count)
+
+        // asa se trimite update-ul la toate socket-urile existente
+        io.emit('countUpdated', count)
+    })
 })
+
 
 server.listen(port, () => {
     console.log('Server is up on port ' + port)
