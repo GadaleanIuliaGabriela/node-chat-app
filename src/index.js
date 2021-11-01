8c8b48987ab1ea483a9c9b9320f64211a68cabec
Iulia Gadalean <gadalean.iulia.gabriela@gmail.com>
2021-11-01 12:10:35 +0200

    initializare socket.io

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 const path = require('path');
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
 const publicDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectory))
+
+
+// event care se numeste connection si care are loc atunci cand un client se conecteaza la server
+io.on('connection', () => {
+    console.log('New wev socket connection')
+
+
+})
 
 server.listen(port, () => {
     console.log('Server is up on port ' + port)
